472ebf186a361e573dfff8e70759dde6fa2d901f
SolielLuimiere <54183914+SolielLumiere09@users.noreply.github.com>
2020-12-11 19:38:53 -0800

    Terminada

---
+++ Assets/Scenes/CommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using IBM.Watsson.Examples;
+using UnityEngine;
+
+public class CommandHandler : MonoBehaviour
+{
+    public static Boolean JUMP_MIKU;
+    public static Boolean RAINBOW_MIKU;
+    public static Boolean DASH_MIKU;
+    public static Boolean RAINBOW_ROAD_MIKU;
+    public static Boolean STOP;
+    public static Boolean DANCE_MIKU;
+    public static Boolean ROLL_FAIL_MIKU;
+    private ExampleStreaming speechListener;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speechListener = GetComponent<ExampleStreaming>();
+        
+        if(speechListener == null)
+            Debug.Log("NULL speedListener");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string command = speechListener.getCommand();
+
+        ROLL_FAIL_MIKU = command.Equals("rolling");
+        DANCE_MIKU = command.Equals("dancing");
+        STOP = command.Equals("stop");
+        JUMP_MIKU = command.Equals("jump");
+        RAINBOW_MIKU = command.Equals("rainbow");
+        DASH_MIKU = command.Equals("she walks");
+        RAINBOW_ROAD_MIKU = command.Equals("rainbow road") || command.Equals("rainbow rows");
+    }
+
+
+}
---
+++ Assets/Scenes/Sprites/Animations/HatsuneMiku/MukuController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MukuController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private AudioSource _audioSource;
+    
+    public AudioClip rainbowRoad;
+    
+    
+    
+    public float jumpForce = 1;
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CommandHandler.JUMP_MIKU)
+            _animator.Play("jump");
+        if (CommandHandler.RAINBOW_ROAD_MIKU)
+        {
+            _audioSource.clip = rainbowRoad;
+            _audioSource.Play();
+            _animator.Play("rainbow");
+        }
+        if(CommandHandler.ROLL_FAIL_MIKU)
+            _animator.Play("roll");
+        if(CommandHandler.DASH_MIKU)
+            _animator.Play("dash");
+        
+        if(CommandHandler.DANCE_MIKU)
+            _animator.Play("dancing");
+        
+        if (CommandHandler.STOP)
+        {
+            _audioSource.Stop();
+            _animator.Play("Idle");
+        }
+    }
+
+    public void Jump()
+    {
+        _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void OnMouseDown()
+    {
+        _audioSource.Stop();
+        _animator.Play("Idle");
+
+    }
+}
